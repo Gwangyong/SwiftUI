5adf5996572c596acfc43d609e9e08c2fb501c19
Gwangyong <key4168@icloud.com>
2025-12-21 21:52:01 +0900

    readme 및 스택뷰 사용 기본 레이아웃 설정

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+    VStack(alignment: .leading) {
+      Text("Turtle Rock")
+        .font(.title)
+      HStack{
+        Text("Joshua Tree National Park")
+          .font(.subheadline)
+        Spacer() // 보이지 않지만 남는 공간을 전부 다 차지해서 다른 뷰를 밀어내는 역할
+        Text("California")
+          .font(.subheadline)
+      }
     }
-    .padding()
+    .padding() // 스세팀이 알아서 주는 기본 여백
   }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# SwiftUI
+## SwiftUI
+
+- SwiftUI 관련 학습을 정리하기 위한 레포
