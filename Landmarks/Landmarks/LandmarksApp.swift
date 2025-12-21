409c462c9624d7a7932c59b51329cbfca88075dc
Gwangyong <key4168@icloud.com>
2025-12-21 21:15:25 +0900

    SwiftUI 초기

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 서광용 on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
