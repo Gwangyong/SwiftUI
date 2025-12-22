e7ebdff8ec1008b4396c0d934cb8142ca954a2e3
Gwangyong <key4168@icloud.com>
2025-12-22 22:40:54 +0900

    JSON 기반 Landmark 데이터 로딩 추가 - 번들 JSON 파일을 Decodable 모델로 디코딩 - ModelData에서 Landmark 데이터 초기화

---
+++ Landmarks/Landmarks/ModelData.swift
@@ -0,0 +1,33 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by 서광용 on 12/22/25.
+//
+
+import Foundation
+
+var landmark: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+  let data: Data
+  
+  // 앱 번들 내에서 filename에 해당하는 파일 URL 찾기
+  guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+    fatalError("Couldn't find \(filename) in main bundle.")
+  }
+  
+  do {
+    // 파일 -> Data 타입으로 로딩
+    data = try Data(contentsOf: file)
+  } catch {
+    fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+  }
+  
+  do {
+    let decoder = JSONDecoder()
+    return try decoder.decode(T.self, from: data) // JSON 데이터를 T 타입으로 디코딩
+  } catch {
+    fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+  }
+}
