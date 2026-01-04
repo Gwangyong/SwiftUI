3c3b819885ae5077e5e1d7503ce90beab597c716
Gwangyong <key4168@icloud.com>
2026-01-04 22:09:33 +0900

    SwiftUI의 Identifiable에 대해 배운점 주석 정리

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -18,3 +18,17 @@ struct LandmarkList: View {
 #Preview {
   LandmarkList()
 }
+
+// MARK: - Learned
+
+// SwiftUI List는 UIKit의 UITableView처럼 index 기반으로 동작하지 않음.
+// 각 row를 "몇 번째"가 아니라, "누구인지(Identity)"로 구분
+// 그래서 List(landmarks)를 사용할때, 요소가 Identifiable을 채택하거나, id 기준(KeyPath)을 명시해줘야함.
+//
+// UIKit처럼 forEach 개념으로 자동 순회할거라 생각했지만,
+// SwiftUI는 diff 기반 렌더링이라 id가 없으면 에러가 발생함.
+//
+// 추가로, id 대신 "중복되지 않는다는 보장"이 있는 name을 기준으로도 가능.
+// 다만 name은 값 변경이나 중복 가능성이 있으므로 안전하지 않음
+// SwiftUI가 원하는 것은 id 자체가 아니라, 각 요소를 항상 유일하게 식별할 수 있는 "고유성"이며,
+// 이거슬 보장하기 위해 Identifiable를 채택하는 것이 가장 안전한 방식.
