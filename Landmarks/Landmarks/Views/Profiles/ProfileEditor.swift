2002065115527972f480dba4fcb1bcd30f8c9d93
Gwangyong <key4168@icloud.com>
2026-01-29 22:49:11 +0900

    Section 3. Define the profile editor - DatePicker에서 선택 가능한 dateRange 범위 계산 - 년/월/일만 보이도록 .date로 DatePicker 추가

--- Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
+++ Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
@@ -11,6 +11,13 @@ struct ProfileEditor: View {
   // @Binding을 통해 상위뷰의 profile에 직접 접근해서 읽고/쓰기
   @Binding var profile: Profile
   
+  var dateRange: ClosedRange<Date> { // ClosedRange: 날짜 밤위를 '양 끝까지 포함'
+    // goalDate 기준 정확히 1년전(-1, .year)날짜 계산
+    let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+    let max = Calendar.current.date(byAdding: .year, value: +1, to: profile.goalDate)!
+    return min...max // goalDate 기준 1년 전/후 날짜 범위
+  }
+  
   var body: some View {
     List {
       HStack {
@@ -35,6 +42,13 @@ struct ProfileEditor: View {
           // Text는 화면에 보이는 글자, tag는 선택시 '저장되는 실제 값'
           Text(season.rawValue).tag(season)
         }
+      }
+      
+      // selection: 바꿀 대상 값, dateRange: 선택 가능한 날짜 범위 제한
+      // displayedComponents: 보여줄 구성요소(.date: 날짜만, .hourAndMinute 시간만)
+      // [.date, .hourAndMinute]로 날짜 + 시간 둘 다 표시 가능
+      DatePicker(selection: $profile.goalDate, in: dateRange, displayedComponents: .date) {
+        Text("Goal Date")
       }
     }
   }
