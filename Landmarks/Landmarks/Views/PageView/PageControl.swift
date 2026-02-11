9344fe6c29a9d1f4116290e5bda20d22d5f1b2c1
Gwangyong <key4168@icloud.com>
2026-02-12 00:39:27 +0900

    Section 4. Add a custom page control - Step 3~6 - PageControl에 update를 위한 Coordinator 생성 - addTarget으로 값 변경시 update 수행 - CategoryHome에서 임시 표시 이미지를 pageView로 변경

--- Landmarks/Landmarks/Views/PageView/PageControl.swift
+++ Landmarks/Landmarks/Views/PageView/PageControl.swift
@@ -12,15 +12,36 @@ struct PageControl: UIViewRepresentable {
   var numberOfPages: Int
   @Binding var currentPage: Int
   
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
+  }
+  
   func makeUIView(context: Context) -> UIPageControl {
     let control = UIPageControl() // UIKit 객체
     control.numberOfPages = numberOfPages // SwiftUI값 -> UIKit 프로퍼티 복사
+    control.addTarget( // UIKit 이벤트 SwiftUI 상태로 전달. 연결코드
+      context.coordinator,
+      action: #selector(Coordinator.updateCurrentPage(sender:)),
+      for: .valueChanged) // 점 눌러서 UIPageControl 페이지 변경되었을 경우
     
     return control
   }
   
   func updateUIView(_ uiView: UIPageControl, context: Context) {
     uiView.currentPage = currentPage
+  }
+  
+  class Coordinator: NSObject {
+    var control: PageControl
+    
+    init(_ control: PageControl) {
+      self.control = control
+    }
+    
+    @objc
+    func updateCurrentPage(sender: UIPageControl) {
+      control.currentPage = sender.currentPage
+    }
   }
 }
 
